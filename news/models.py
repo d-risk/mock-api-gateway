adc90cc1d4347a91fed3d0fbc7236b49effdf9e5
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-03-04 10:54:26 +0800

    enable indexing for date_time
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- news/models.py
+++ news/models.py
@@ -1,14 +1,14 @@
-from django.db.models import Model, TextField, URLField, DateTimeField, UUIDField
+from django.db import models
 from django.utils.timezone import now
 
 
 # Create your models here.
-class News(Model):
-    company_id = UUIDField(db_index=True, editable=False, )
-    title = TextField(db_index=True, editable=False, )
-    date_time = DateTimeField(default=now, editable=False, )
-    snippet = TextField(editable=False, )
-    url = URLField(editable=False, )
+class News(models.Model):
+    company_id = models.UUIDField(db_index=True, editable=False, )
+    title = models.TextField(db_index=True, editable=False, )
+    date_time = models.DateTimeField(db_index=True, default=now, editable=False, )
+    snippet = models.TextField(editable=False, )
+    url = models.URLField(editable=False, )
 
     class Meta:
         db_table = 'app_news'
