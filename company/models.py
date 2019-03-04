ca2988d01be2d25039fecf42c0f0ef261759325a
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-03-04 14:05:41 +0800

    set all fields to be non-editable
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- company/models.py
+++ company/models.py
@@ -7,12 +7,12 @@ from django_countries.fields import CountryField
 # Annex F - Company Data Service
 # SQL data model
 class Company(Model):
-    id = UUIDField(default=uuid.uuid4, editable=False, primary_key=True, )
-    name = TextField(db_index=True, default=None, unique=True, )
-    industry = TextField(db_index=True, default=None, )
-    description = TextField(default=None, )
-    exchange = TextField(db_index=True, default=None, )
-    country = CountryField(db_index=True, default=None, )
+    company_id = UUIDField(default=uuid.uuid4, editable=False, primary_key=True, )
+    name = TextField(db_index=True, default=None, unique=True, editable=False, )
+    industry = TextField(db_index=True, default=None, editable=False, )
+    description = TextField(default=None, editable=False, )
+    exchange = TextField(db_index=True, default=None, editable=False, )
+    country = CountryField(db_index=True, default=None, editable=False, )
 
     class Meta:
         db_table = 'app_companies'
