5df9f49a3e63df8692e1bb111d43bfed247621f2
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-03-04 15:04:32 +0800

    clarify the usage of models
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- company/models.py
+++ company/models.py
@@ -1,18 +1,18 @@
 import uuid
 
-from django.db.models import Model, UUIDField, TextField
-from django_countries.fields import CountryField
+from django.db import models
+from django_countries import fields
 
 
 # Annex F - Company Data Service
 # SQL data model
-class Company(Model):
-    company_id = UUIDField(default=uuid.uuid4, editable=False, primary_key=True, )
-    name = TextField(db_index=True, default=None, unique=True, editable=False, )
-    industry = TextField(db_index=True, default=None, editable=False, )
-    description = TextField(default=None, editable=False, )
-    exchange = TextField(db_index=True, default=None, editable=False, )
-    country = CountryField(db_index=True, default=None, editable=False, )
+class Company(models.Model):
+    company_id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True, )
+    name = models.TextField(db_index=True, default=None, unique=True, editable=False, )
+    industry = models.TextField(db_index=True, default=None, editable=False, )
+    description = models.TextField(default=None, editable=False, )
+    exchange = models.TextField(db_index=True, default=None, editable=False, )
+    country = fields.CountryField(db_index=True, default=None, editable=False, )
 
     class Meta:
         db_table = 'app_companies'
