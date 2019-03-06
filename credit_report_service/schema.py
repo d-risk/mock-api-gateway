15641dd4ecbbba4ca79a2476546f1febb453f209
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-03-06 17:36:07 +0800

    update database creation code to the new schema
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- credit_report_service/schema.py
+++ credit_report_service/schema.py
@@ -2,9 +2,11 @@ from graphene import Schema, ObjectType
 
 from company.schema import CompanyQuery
 from credit_report.schema import CreditReportQuery
+from financial_report.schema import FinancialReportQuery
+from news.schema import NewsQuery
 
 
-class Query(CompanyQuery, CreditReportQuery, ObjectType):
+class Query(CompanyQuery, CreditReportQuery, FinancialReportQuery, NewsQuery, ObjectType):
     pass
 
 
