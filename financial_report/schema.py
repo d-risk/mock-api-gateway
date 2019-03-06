147ad394052d3bb059073e58fd35c04e50a43af1
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-03-06 14:14:35 +0800

    update financial report schema
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- financial_report/schema.py
+++ financial_report/schema.py
@@ -1,53 +1,108 @@
-from django_filters import FilterSet, NumberFilter
-from graphene import Node, ObjectType
-from graphene_django import DjangoObjectType
-from graphene_django.filter import DjangoFilterConnectionField
+import logging
 
-import financial_report.models
-from financial_report.models import FinancialReport
+import django_filters
+import graphene
+import graphene_django
+import graphql
+from graphene import relay
+from graphene_django import filter
+
+from financial_report.models import FinancialData as FinancialDataModel
+from financial_report.models import FinancialRatio as FinancialRatioModel
+from financial_report.models import FinancialReport as FinancialReportModel
 
 
-class FinancialReportFilter(FilterSet):
+class FinancialReportFilter(django_filters.FilterSet):
+    company_id = django_filters.UUIDFilter(required=True, label='The UUID of the company', )
+    year = django_filters.NumberFilter(
+        field_name='date_time',
+        lookup_expr='year',
+        label='Filter out financial reports that does not match the year',
+    )
+    from_year = django_filters.NumberFilter(
+        field_name='date_time',
+        lookup_expr='year__gte',
+        label='Filter out financial reports that are older than the year',
+    )
+    to_year = django_filters.NumberFilter(
+        field_name='date_time',
+        lookup_expr='year__lte',
+        label='Filter out financial reports that newer than the year',
+    )
+
     class Meta:
-        model = FinancialReport
-        fields = ['date_time', ]
-
-    year = NumberFilter(field_name='date_time', lookup_expr='year')
-    from_year = NumberFilter(field_name='date_time', lookup_expr='year__gte')
-    to_year = NumberFilter(field_name='date_time', lookup_expr='year__lte')
+        model = FinancialReportModel
+        fields = ['company_id', ]
 
     @property
-    def qs(self: FilterSet):
-        if not hasattr(self, '_qs'):
-            qs = self.queryset.all().order_by('-date_time')
-            if self.is_bound:
-                # ensure form validation before filtering
-                self.errors
-                qs = self.filter_queryset(qs)
-            self._qs = qs
-        return self._qs
+    def qs(self):
+        return super(FinancialReportFilter, self).qs.order_by('-date_time', )
 
 
-class FinancialReportNode(DjangoObjectType):
+class FinancialData(graphene_django.DjangoObjectType):
+    id = graphene.ID(description='The ID of the financial data', required=True, )
+    name = graphene.String(description='The name of the financial data', required=True, )
+    value = graphene.Float(description='The value of the financial data', required=True, )
+
     class Meta:
-        model = FinancialReport
-        interfaces = [Node, ]
+        model = FinancialDataModel
+        description = 'A financial data'
 
 
-class FinancialData(DjangoObjectType):
+class FinancialRatio(graphene_django.DjangoObjectType):
+    id = graphene.ID(description='The ID of the financial ratio', required=True, )
+    name = graphene.String(description='The name of the financial ratio', required=True, )
+    value = graphene.Float(description='The value of the financial ratio', required=True, )
+    formula = graphene.String(description='The formula to calculate the value', required=True, )
+
     class Meta:
-        model = financial_report.models.FinancialData
+        model = FinancialRatioModel
+        description = 'A financial ratio'
 
 
-class FinancialRatio(DjangoObjectType):
+class FinancialReport(graphene_django.DjangoObjectType):
+    report_id = graphene.ID(description='The ID of the financial report', required=True, )
+    company_id = graphene.UUID(
+        description='The company, as identified by the UUID, of the financial report',
+        required=True,
+    )
+    date_time = graphene.DateTime(description='The date and time of the report', required=True, )
+    currency = graphene.String(description='The currency that the report uses', required=True, )
+    financial_data = graphene.List(of_type=FinancialData, description='A list of financial data', required=True, )
+    financial_ratios = graphene.List(of_type=FinancialRatio, description='A list of financial ratios', required=True, )
+
     class Meta:
-        model = financial_report.models.FinancialRatio
+        model = FinancialReportModel
+        description = 'A financial report'
 
 
-class FinancialReportQuery(ObjectType):
-    financial_report = Node.Field(FinancialReportNode, )
-    financial_reports = DjangoFilterConnectionField(
-        FinancialReportNode,
-        order_by='-date_time',
+class FinancialReportNode(FinancialReport):
+    id = relay.GlobalID(description='A global ID that relay uses for reactive paging purposes', )
+
+    class Meta:
+        model = FinancialReportModel
+        interfaces = (relay.Node,)
+        description = 'A node that encapsulates the financial report to support data-driven React applications'
+
+
+class FinancialReportQuery(graphene.ObjectType):
+    financial_report = graphene.Field(
+        type=FinancialReport,
+        description='Find a financial report using an ID',
+        report_id=graphene.ID(required=True, description='The ID of a financial report', ),
+    )
+    financial_reports = filter.DjangoFilterConnectionField(
+        type=FinancialReportNode,
+        description='Search for a list of financial report of a company (by the given UUID) that is ordered by date '
+                    'and time',
         filterset_class=FinancialReportFilter,
     )
+
+    def resolve_financial_report(
+            self,
+            info: graphql.ResolveInfo,
+            report_id: graphene.ID,
+            **kwargs,
+    ) -> FinancialReport:
+        logging.debug(f'self={self}, info={info}, news_id={report_id} kwargs={kwargs}')
+        return FinancialReportModel.objects.get(report_id=report_id, )
