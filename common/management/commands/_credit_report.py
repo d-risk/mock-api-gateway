20b850952c9bc40fa1f3ed9085ace079510a9dd3
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-03-06 16:10:52 +0800

    update code to create credit reports with random values
    Signed-off-by: Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>

--- common/management/commands/_credit_report.py
+++ common/management/commands/_credit_report.py
@@ -1,10 +1,34 @@
-from datetime import datetime
+from datetime import datetime, timezone
+from random import randint, choice, uniform
 from typing import List
 
 from company.models import Company
 from credit_report.models import CreditReport
-from financial_report.models import FinancialReport
-from news.models import News
+
+RATINGS: List[str] = [
+    'AAA',
+    'AA+',
+    'AA',
+    'AA-',
+    'A+',
+    'A',
+    'A-',
+    'BBB+',
+    'BBB',
+    'BBB-',
+    'BB+',
+    'BB',
+    'BB-',
+    'B+',
+    'B',
+    'B-',
+    'CCC+',
+    'CCC',
+    'CCC-',
+    'CC',
+    'C',
+    'D',
+]
 
 
 def create_credit_report(
@@ -12,18 +36,31 @@ def create_credit_report(
         probability_of_default: float,
         credit_rating: str,
         date_time: datetime,
-        financial_reports: List[FinancialReport] = None,
-        news: List[News] = None,
 ) -> CreditReport:
     credit_report = CreditReport.objects.create(
-        company_id=company.id,
+        company_id=company.company_id,
         probability_of_default=probability_of_default,
         credit_rating=credit_rating,
         date_time=date_time,
     )
-    if financial_reports:
-        credit_report.financial_reports.set(financial_reports)
-    if news:
-        credit_report.news.set(news)
     print(f'        + Credit Report \'{credit_report.id}\' ({credit_report.date_time}) created', )
     return credit_report
+
+
+def random_credit_report(company: Company, date_time: datetime, ) -> CreditReport:
+    credit_report = create_credit_report(
+        company=company,
+        probability_of_default=uniform(0, 1),
+        credit_rating=choice(RATINGS),
+        date_time=date_time,
+    )
+    return credit_report
+
+
+def random_credit_reports(company: Company, from_year: int, to_year: int) -> List[CreditReport]:
+    credit_reports: List[CreditReport] = []
+    for year in range(from_year, to_year + 1):
+        date_time = datetime(year=year, month=randint(1, 12), day=randint(1, 28), tzinfo=timezone.utc, )
+        credit_report = random_credit_report(company=company, date_time=date_time, )
+        credit_reports.append(credit_report, )
+    return credit_reports
