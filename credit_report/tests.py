29b72db20a4b6b9caab7ed874b92441a00197be3
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-04-11 12:40:20 +0800

    fix error in test case

--- credit_report/tests.py
+++ credit_report/tests.py
@@ -3,6 +3,7 @@ from uuid import uuid4
 from django.test import TestCase
 from django.utils.timezone import now
 
+from credit_rating.models import CreditRating
 from credit_report.models import CreditReport
 
 
@@ -11,7 +12,7 @@ class CreditReportTestCase(TestCase):
     def test_create_credit_report(self):
         company_id = uuid4()
         probability_of_default = 0.1
-        credit_rating = 'CR'
+        credit_rating = CreditRating.AAA.readable_name
         date_time = now()
 
         credit_report = CreditReport.objects.create(
@@ -26,7 +27,3 @@ class CreditReportTestCase(TestCase):
         self.assertEqual(credit_report.company_id, company_id)
         self.assertEqual(credit_report.probability_of_default, probability_of_default)
         self.assertEqual(credit_report.date_time, date_time)
-        self.assertIsNotNone(credit_report.financial_reports)
-        self.assertEqual(credit_report.financial_reports.count(), 0)
-        self.assertIsNotNone(credit_report.news)
-        self.assertEqual(credit_report.news.count(), 0)
