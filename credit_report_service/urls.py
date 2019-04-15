a5b313c0e557866713c192351e914a10d144e243
Christopher Zhong <1946052+ChristopherZhong@users.noreply.github.com>
2019-04-15 19:54:41 +0800

    add url for health check

--- credit_report_service/urls.py
+++ credit_report_service/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path
 from graphene_django.views import GraphQLView
 from typing import List
 
 urlpatterns: List[path] = [
     path('', GraphQLView.as_view(graphiql=True)),
+    path('health-check', HttpResponse),
 ]
